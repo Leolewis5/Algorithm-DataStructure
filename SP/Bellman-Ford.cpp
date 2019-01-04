64e576d39100ecd0d7517bea640864a818778dfe
Leolewis5 <44966464+Leolewis5@users.noreply.github.com>
2019-01-04 16:01:41 +0800

    Add files via upload

--- SP/Bellman-Ford.cpp
+++ SP/Bellman-Ford.cpp
@@ -5,7 +5,7 @@
 
 using namespace std;
 
-int ExtractMin(int* ,bool* , int );
+
 
 struct Edge{
 	int from, to, weight;
@@ -25,78 +25,48 @@ class GraphSP{
 			AdjList.resize(vertex);
 		}
 		void AddEdgeList(int, int, int);
-		void DijkstraSP(int);
+		void BellmanFordSP(int);
 		friend int ExtractMin(int*,bool* , int);
 		
 };
 
 
 
-void GraphSP::DijkstraSP(int start = 0){
-	
-	// allocated predecessor and value and visted array
-	int predecessor[vertex], value[vertex];
-	bool visted[vertex];
-	
-	//initial informantion
-	for (int i=0; i<vertex ;i++){
-		predecessor[i]=-1;
-		value[i]=1000;//maxmun weight, should find a better method to assign it
-		visted[vertex]=false;
+void GraphSP::BellmanFordSP(int start = 0){
+	int Memory[vertex][vertex];
+	Memory[0][start]=0;
+	for( int i=0; i<vertex;i++){
+		if (i!=start)	Memory[0][i]=10000; //regard as infinited
 	}
 	
+	// OPT ( i, v) = min{ OPT (i -1 ,w), min {(i-1, x) + Cwx} }
 	
-	int Checknode = vertex;
-	
-	//start from enter vertex
-	int current = start;
-	visted[current]=true;
-	value[current]=0;
-	Checknode--;
-	
-	// Dijkstra's visted
-	while(Checknode != 0){
-		for (list<Edge>::iterator itr = AdjList[current].begin(); itr != AdjList[current].end(); itr ++){
-			if (visted[(*itr).to]==false&&value[(*itr).to]>(*itr).weight+value[current]) {
-				value[(*itr).to] = (*itr).weight+value[current];
-				predecessor[(*itr).to] = current;
+	for (int i=1;i<vertex;i++){ // control distance 
+		for (int j=0; j<vertex;j++) //control vertex shmoo
+			Memory[i][j] = Memory[i-1][j];
+			
+		for (int j=0; j<vertex;j++ ){ //control vertex shmoo
+			for (list<Edge>:: iterator itr = AdjList[j].begin(); itr != AdjList[j].end(); itr++){
+				if ((Memory[i][(*itr).to])>((*itr).weight+Memory[i-1][j]))
+					Memory[i][(*itr).to]=((*itr).weight+Memory[i-1][j]) ;		 	
 			}
 		}
-
-		int extract = ExtractMin(value, visted, vertex);
-		
-		visted[extract] = true;
-		current = extract;
-		Checknode--;
 	}
-			
-
 	
 	// printSP
 	cout << "begin from: "<< start <<" to each node which weight: "<<endl;
-	for (int i=0; i< vertex; i++){
-		cout << "vertex:" << i <<" , which weight is: "<<setw(3)<<value[i]<<endl;
-	}
-}
-
-int ExtractMin(int * value, bool* visted,  int vertex){
-	int min = 1000;
-	int extract=0;
-	for (int i=0; i<vertex; i++){
-		if (visted[i]==false&&value[i]<min) {
-			min = value[i];
-			extract = i;
+	for (int i=0 ; i < vertex; i++){
+		for (int j=0; j< vertex; j++){
+			cout << setw(10)<< Memory[j][i];
 		}
+		cout<< endl;
 	}
-	return extract;
 }
-
 
 void GraphSP::AddEdgeList(int from, int end, int weight){
 	Edge NewEdge(from,end,weight);
 	AdjList[from].push_back(NewEdge);
 }
-
 
 
 
@@ -112,20 +82,28 @@ int main(){
     G.AddEdgeList(5, 0, 3);G.AddEdgeList(5, 4, 6);
     G.AddEdgeList(6, 1, 4);G.AddEdgeList(6, 2, 8);G.AddEdgeList(6, 3, 9);G.AddEdgeList(6, 4, 2);
 	*/
-
-
+	
+	/*
     GraphSP G(6);
     G.AddEdgeList(0, 1, 8);G.AddEdgeList(0, 5, 1);
     G.AddEdgeList(1, 0, 3);G.AddEdgeList(1, 2, 1);
     G.AddEdgeList(2, 0, 5);G.AddEdgeList(2, 3, 2);G.AddEdgeList(2, 4, 2);
     G.AddEdgeList(3, 1, 4);G.AddEdgeList(3, 2, 6);G.AddEdgeList(3, 4, 7);G.AddEdgeList(3, 5, 3);
     G.AddEdgeList(5, 3, 2);G.AddEdgeList(5, 4, 8);
-   
-
+	*/
+	
+	// try negetive weight
+    GraphSP G(6);
+    G.AddEdgeList(0, 1, -3);G.AddEdgeList(0, 3, 3);G.AddEdgeList(0, 4, 4);G.AddEdgeList(0, 5, 2);
+    G.AddEdgeList(1, 4, 6);
+    G.AddEdgeList(2, 1, -4);G.AddEdgeList(2, 3, 8);
+    G.AddEdgeList(3, 5, -3);
+    G.AddEdgeList(4, 2, -1);
+    G.AddEdgeList(5, 2, -2);
 
 
     std::cout << "SP found by Dijkstra :\n";
-    G.DijkstraSP();
+    G.BellmanFordSP();
 
 	
 	return 0;
